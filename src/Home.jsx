c3b9dd61ee6b7b02ad2f996cf5afd65fcf5de72a
isharadasuni <isharadasuni160@gmail.com>
2024-08-23 21:26:26 +0530

    Insert CV,Git links and Linkedin profile

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import "./home.scss";
 import Image1 from "../src/Image/image1.JPG";
+import LinkedIn from "../src/Image/LinkedIn.png";
+import GitHub from "../src/Image/GitHub.png";
+import PDF from "../src/Doc/Resume - Ishara Dasuni Senadheera.pdf";
 
 const Home = () => {
 
@@ -14,7 +17,7 @@ const Home = () => {
             <div className="navBar">
 
                 <div className="name">
-                  ISHARA DASUNI SENADHEERA
+                    ISHARA DASUNI SENADHEERA
                 </div>
 
                 <div className="tabs">
@@ -26,25 +29,45 @@ const Home = () => {
 
             </div>
 
-<hr/>
+
+            <hr />
+
 
             <div className="profile">
 
                 <div className="image">
-                    <img src={Image1} alt="profile image"/>
-
+                    <img className="img1" src={Image1} alt="profile image" />
                 </div>
 
                 <div className="info">
                     <p className="infoHead">Hello, I'm </p>
                     <p className="infoName">Ishara Dasuni Senadheera</p>
                     <p className="infoRole">Software Engineer</p>
-                    <button className="download">Download CV</button>
+
+                    <a href={PDF} download="Resume - Ishara Dasuni Senadheera.pdf">
+                        <button className="download">Download CV</button>
+                    </a>
+
                     <button className="contact">Contact Info</button>
+
+                    <p>
+                        <a href="https://www.linkedin.com/in/ishara-dasuni-senadheera-12207820b/" target="_blank">
+                        <img className="img2" src={LinkedIn} alt="LinkedIn" />
+                        </a>
+                        <a href="https://github.com/isharadasuni" target="_blank">
+                        <img className="img2" src={GitHub} alt="GitHub" />
+                        </a>
+                    </p>
+
 
                 </div>
 
+
             </div>
+
+            <hr />
+
+
 
 
 
